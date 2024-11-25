262d780e30846bb3f48b8f5040037b7eb7557e2f
Cristianmax-Perez <cristian_max2005@yahoo.com.ar>
2024-11-25 20:37:57 -0300

    envio 2 2511

---
+++ proyectorym/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Home () {
+    return (
+        <div>
+            <h1>Proyect Rick & Morty</h1>
+            <h2>Welcome to Rick & Morty Project</h2>
+            <p>This proyect was made for practising React and to made a functional website</p>
+            <p>In this website you can know information of the characters of this animated series.</p>
+            <p>Also you can filter for diferent types of properties to find the character that you are looking for or send us a massage for any concern o sugestion</p>
+            <h2>Let's go!</h2>
+        </div>
+    )
+}
